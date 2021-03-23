62a12d5ceba34ca9ec58a9debdbe6a7360efaf08
obsoleteguy <theohainesdev@gmail.com>
2021-03-23 21:20:39 +0000

    added launcher

---
+++ invinciable browser/launcher.bat
@@ -0,0 +1,15 @@
+start browser-custom.exe
+
+echo _            _            _ _     _      
+echo(_)          (_)          (_) |   | |     
+echo _ _ ____   ___ _ __   ___ _| |__ | | ___ 
+echo| | '_ \ \ / / | '_ \ / __| | '_ \| |/ _ \
+echo| | | | \ V /| | | | | (__| | |_) | |  __/
+echo|_|_| |_|\_/ |_|_| |_|\___|_|_.__/|_|\___|
+echo by obsolete_dev
+
+exit /b
+@pause
+pause
+
+launcher /k
