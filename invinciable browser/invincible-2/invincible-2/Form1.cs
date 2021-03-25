5163bbfa3164e6c9c30d323d2d31d81164902379
obsoleteguy <theohainesdev@gmail.com>
2021-03-25 16:48:50 +0000

    added source code feel free to use

---
+++ invinciable browser/invincible-2/invincible-2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+
+namespace invincible_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void webView21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webviewpanel.CoreWebView2.Navigate("https://www.google.com");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webviewpanel.CoreWebView2.Navigate("https://" + textBox1.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webviewpanel.CoreWebView2.GoForward();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            webviewpanel.CoreWebView2.GoBack();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webviewpanel.CoreWebView2.Reload();
+        }
+    }
+}
