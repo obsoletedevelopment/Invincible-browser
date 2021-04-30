e694309446df68ed84e36aa9c7139b80815743c0
obsoleteguy <81994749+susan-says@users.noreply.github.com>
2021-04-30 18:22:12 +0100

    ok

--- invinciable browser/Invincible launcher/Invincible launcher/Form1.Designer.cs
+++ invinciable browser/Invincible launcher/Invincible launcher/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-6, -2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(810, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "launch browser";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "invincible launcher";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
